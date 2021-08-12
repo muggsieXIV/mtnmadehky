3f09d848c15daca8e8cd2f36fb1c3d95212528f7
muggsieXIV <55777246+muggsieXIV@users.noreply.github.com>
2021-08-12 17:18:02 -0600

    Update Settings.py

--- mtnmadehky/settings.py
+++ mtnmadehky/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'a#8@f&6np4k@bv_1ki6c88hh95f1zw^sjn37m@extzj1f79fk3'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
